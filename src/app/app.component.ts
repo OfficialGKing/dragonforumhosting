50bd35c1e54f76b1e7822762b36b0c0038dfd4cd
BuildTools <unconfigured@null.spigotmc.org>
2018-04-26 15:30:37 -0700

    Added Angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Dragon Forum Hosting';
+  description = "Welcome to the internet's friendest forum hosting service!"
+}
